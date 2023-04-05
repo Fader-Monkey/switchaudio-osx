900d2d69b6aa93b112b39f1554f4d672939725ce
Clark Rochon <clark.rochon@northpointministries.net>
2023-04-05 13:55:29 -0400

    headphones

---
+++ external-headphones-fix-v4.sh
@@ -0,0 +1,4 @@
+while command
+do
+        ~/switchaudio-osx/build/Release/SwitchAudioSource -s External\ Headphones; ~/switchaudio-osx/build/Release/SwitchAudioSource -s External\ Headphones -t input ; sleep 10
+done

5a7ae6126b290173837b6c68439ec40cb902ca58
Clark Rochon <clarkrockon@gmail.com>
2023-04-06 12:10:17 -0400

    Delete installer v3.sh

--- installer v3.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#accepts xcode licence if not done already
-sudo xcodebuild -license accept
-cd /Users/Shared/
-# downloads switch audio osx
-git clone https://github.com/Fader-Monkey/switchaudio-osx.git
-cd /Users/Shared/switchaudio-osx/
-#builds from source code
-make build
